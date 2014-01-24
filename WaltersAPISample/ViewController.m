00bd484dcd7e5e1323dbe1843d2266ed1289e240
Ed Schmalzle <ed@nerded.net>
2014-01-23 22:09:46 -0500

    update sample to utilize single object fetch

--- WaltersAPISample/ViewController.m
+++ WaltersAPISample/ViewController.m
@@ -18,13 +18,20 @@
 {
     [super viewDidLoad];
     TWAClient *client = [[TWAClient alloc] initWithAPIKey: @"sOK3xgGkv3ooeK4J9P5yKINSs8vSgWEMSeLp6TLUkT6iX7B5hraqLDEZ1sBpln3O"];
-    TWARequestOptions *requestOptions = [TWARequestOptions defaultOptions];
+    TWARequestOptions *requestOptions = [[TWARequestOptions alloc] init];
+    
     [client getObjectsWithRequestOptions: requestOptions
                                     success: ^(NSArray* objectList) {
                                         NSLog(@"I win");
                                     } fail: ^(NSURLResponse *response, NSError *error) {
                                         NSLog(@"I lose");
                                     }];
+    
+    [client getObjectByID: @"2" onSuccess:^(id item) {
+        NSLog(@"Second win");
+    } fail:^(NSURLResponse *response, NSError *error) {
+        NSLog(@"Second fail");
+    }];
 }
 
 - (void)didReceiveMemoryWarning
